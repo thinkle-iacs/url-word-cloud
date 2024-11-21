4631cbb98891aaf0b3a1ba0218de51b0ee4fbedf
Tom Hinkle <tmhinkle@gmail.com>
2024-11-21 14:37:57 -0500

    boilerplate

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; // Add any global CSS styles here.
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
