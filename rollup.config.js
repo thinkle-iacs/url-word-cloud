3413592964deb367895b4fddbd00a71120483d65
Tom Hinkle <tmhinkle@gmail.com>
2024-11-24 09:55:14 -0500

    Correct placement of libraries we serve

--- rollup.config.js
+++ rollup.config.js
@@ -6,14 +6,31 @@ import terser from "@rollup/plugin-terser";
 
 export default {
   input: "src/utils/urlBuilder.js", // Path to your entry file
-  output: {
-    file: "public/urlBuilder.js", // Output bundle
-    format: "esm", // ES Module format
-    sourcemap: true, // Optional: Generates a sourcemap
-  },
+  output: [
+    {
+      file: "public/urlBuilder.js", // Output bundle
+      format: "esm", // ES Module format
+      sourcemap: true, // Optional: Generates a sourcemap
+    },
+
+    // CommonJS for Node.js or bundlers
+    {
+      file: "public/urlBuilder.cjs.js",
+      format: "cjs",
+      sourcemap: true
+    },
+
+    // IIFE that attaches `UrlBuilder` to `window`
+    {
+      file: "public/urlBuilder.iife.js",
+      format: "iife",
+      name: "UrlBuilder", // Global object name on `window`
+      sourcemap: true
+    }
+  ],
   plugins: [
     resolve(), // Resolves node_modules
     commonjs(), // Converts CommonJS to ES6
     terser(), // Minifies the bundle
-  ],
+  ]
 };
