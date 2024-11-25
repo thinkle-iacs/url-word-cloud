1274c214bce0f86888fbcc398c61bb8da7c0e9ed
Tom Hinkle <tmhinkle@gmail.com>
2024-11-25 07:33:21 -0500

    Build URL into built JS library

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import terser from "@rollup/plugin-terser";
+import replace from "@rollup/plugin-replace";
 
 export default {
   input: "src/utils/urlBuilder.js", // Path to your entry file
@@ -17,7 +18,7 @@ export default {
     {
       file: "public/urlBuilder.cjs.js",
       format: "cjs",
-      sourcemap: true
+      sourcemap: true,
     },
 
     // IIFE that attaches `UrlBuilder` to `window`
@@ -25,12 +26,18 @@ export default {
       file: "public/urlBuilder.iife.js",
       format: "iife",
       name: "UrlWordCloud", // Global object name on `window`
-      sourcemap: true
-    }
+      sourcemap: true,
+    },
   ],
   plugins: [
+    replace({
+      preventAssignment: true, // Necessary to avoid warnings
+      "process.env.REACT_APP_BASE_URL": JSON.stringify(
+        process.env.REACT_APP_BASE_URL || "http://localhost:3000"
+      ),
+    }),
     resolve(), // Resolves node_modules
     commonjs(), // Converts CommonJS to ES6
     terser(), // Minifies the bundle
-  ]
+  ],
 };
