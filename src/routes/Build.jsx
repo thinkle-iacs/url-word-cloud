0f66b6154fcad0186500494ab63516dda99eefbe
Tom Hinkle <tmhinkle@gmail.com>
2024-11-25 07:22:55 -0500

    add home link

--- src/routes/Build.jsx
+++ src/routes/Build.jsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import Builder from '../components/builder/Builder';
 import { parseParams } from '../utils/parseParams';
+import { Link } from 'react-router-dom';
+import { FiHome } from 'react-icons/fi';
 
 const Build = () => {
   const params = parseParams(window.location.search);
@@ -9,7 +11,8 @@ const Build = () => {
   const initialState = { settings: params, words: params.words };
 
   return (
-    <div class="container mx-auto bg-background text-foreground">      
+    <div class="container mx-auto bg-background text-foreground">    
+      <Link to="/" className="home-link"><FiHome/></Link>  
       <Builder initialState={initialState} />
     </div>
   );
